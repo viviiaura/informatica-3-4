995bcc2b0da23050753dcbbae6b099a22aa9175c
viviiaura <victoriaau23@gmail.com>
2026-04-21 21:58:46 +0000

    ingredients

--- recipe.c
+++ recipe.c
@@ -3,4 +3,7 @@
 int main(void){
     printf("Coconut Cranberry Dessert\n");
     printf("\n");
+    printf("1 1/2 cups    Graham cracker crumbs\n");
+    printf("\n");
+    printf("1/2 cup butter or margarine, melted\n1 can (12 ounces/340 g) sweetened condensed milk");
 }
