1e9b1518c9f9e59c55fdf5a67bc3e583465b621f
viviiaura <victoriaau23@gmail.com>
2026-04-24 14:58:46 +0000

    color

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void)
-{
-    printf("Hello Victoria! \n");
-}
+int main(void){
+    char name[30];
+    char color[15];
+    printf("What is your name?: %s");
+    scanf("%s", &name);
+    printf("Hello %s! \n", name);
+    printf("What is your fav color?: %s ");
+    scanf("%s", &color);
+    printf("Your fav color is: %s \n", color);
+    }
