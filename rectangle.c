f3a3265bc1f39167ae2b192b481a1b1750ba578b
viviiaura <victoriaau23@gmail.com>
2026-04-24 17:18:13 +0000

    rectangle angle and perimeter

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+
+    int width;
+    int length;
+    printf("Enter width: ");
+    scanf("%d", &width);
+    printf("Enter length: ");
+    scanf("%d", &length);
+    printf("Area %d\n", width*length);
+    printf("Perimeter %d\n", 2*(width+length));
+}
