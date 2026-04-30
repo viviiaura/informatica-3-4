0dac58194891ad3c993d30d79af2cb567217dfce
viviiaura <victoriaau23@gmail.com>
2026-04-30 15:37:37 +0000

    fixed the prob, now theres a new one

--- WeeklyWeekly2/minutes.c
+++ WeeklyWeekly2/minutes.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 int main(void){
     int movrunt;
-    int startt;
-    printf ("How long is the movie in minutes? ");
-    scanf("%d", &movrunt);
-    printf("What is the starting time? \n");
-    scanf("%d", &startt);
-    int hrt = movrunt/60;
-    int mint = hrt %1; // sum wrong here
-    int endt = startt+hrt+mint;
-    int endtm = endt %1;
 
-    printf("The movie is %d hr long\n", hrt);
-    printf("%d min long\n", mint);
-    printf("The end time is %d \n", endt);
-    printf(":%d \n", endtm);
+
+    printf ("How long is the movie in minutes? \n");
+    scanf("%d", &movrunt);
+
+    int startHour;
+    int startmin;
+
+    printf("What is the starting time? \n");
+    scanf("%d %d", &startHour, &startmin);
+
+    int hrt = movrunt / 60;
+    int mint = movrunt % 60;
+    int endt = startHour + startmin;
+    int endtm = startmin + mint;
+
+    printf("The movie is %d hr long and %d min\n", hrt, mint);
+    printf("The end time is %d:%d \n", endt, endtm);
 
 }
