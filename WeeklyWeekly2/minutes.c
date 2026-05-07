0953a8a822dd6feca1f8c790d5abd4efd8573169
viviiaura <victoriaau23@gmail.com>
2026-05-07 19:25:24 +0000

    minutes

--- WeeklyWeekly2/minutes.c
+++ WeeklyWeekly2/minutes.c
@@ -2,22 +2,26 @@
 int main(void){
     int movrunt;
 
-
-    printf ("How long is the movie in minutes? \n");
+    printf ("How long is the movie in minutes? ");
     scanf("%d", &movrunt);
 
     int startHour;
     int startmin;
 
-    printf("What is the starting time? \n");
-    scanf("%d %d", &startHour, &startmin);
+    printf("What is the starting time? ");
+    scanf("%d:%d", &startHour, &startmin);
 
     int hrt = movrunt / 60;
     int mint = movrunt % 60;
-    int endt = startHour + startmin;
+    int endt = startHour + hrt;
     int endtm = startmin + mint;
 
+    if(endtm >= 60){
+        endt = endt + (endtm / 60); //Update value of endt variable
+        endtm = endtm % 60;
+    }
+
     printf("The movie is %d hr long and %d min\n", hrt, mint);
-    printf("The end time is %d:%d \n", endt, endtm);
+    printf("The end time is %d:%02d \n", endt, endtm);
 
 }
